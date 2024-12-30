fb8403722722ca21c03de93229e04e035af28596
Liliane <lilianemezani99@gmail.com>
2024-12-30 17:20:23 +0100

    Fix : Resolve conflits before merging to develop

---
+++ frontend/app/page.tsx
@@ -0,0 +1,10 @@
+import Footer from "@/components/Footer";
+import Navbar from "@/components/navbar/Navbar";
+
+export default function Home() {
+  return <main>
+    <Navbar />
+    <h1 className='text-8xl'>Bienvenu !!</h1>
+    <Footer />
+  </main>
+}
---
+++ frontend/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <footer className="bg-gray-800 text-white">
+            <h1 className="text-8xl">Footer</h1>
+        </footer>
+    )
+}
