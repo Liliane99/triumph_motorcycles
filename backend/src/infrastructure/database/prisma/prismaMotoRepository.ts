10bf8993456cd1741041a299fbf6d5e7fae9f761
Liliane <lilianemezani99@gmail.com>
2025-01-31 12:46:13 +0100

    feat : creat routes and test with postman for motorcycles

--- backend/src/infrastructure/database/prisma/prismaMotoRepository.ts
+++ backend/src/infrastructure/database/prisma/prismaMotoRepository.ts
@@ -1,25 +1,27 @@
 import { PrismaClient } from "@prisma/client";
+import { IMotorcycleRepository } from "../../../application/ports/repositories/IMotorcycleRepository";
 import { Moto } from "../../../domain/entities/Motorcycle";
-import { MotoRepository } from "../../../domain/repositories/MotorcycleRepository";
 
 const prisma = new PrismaClient();
 
-export class PrismaMotoRepository implements MotoRepository {
-  async create(moto: Moto): Promise<Moto> {
-    const newMoto = await prisma.moto.create({
-      data: {
-        id: moto.id,
-        brand: moto.brand,
-        model: moto.model,
-        year: moto.year,
-        licensePlate: moto.licensePlate,
-        kilometers: moto.kilometers,
-      },
-    });
-    return newMoto;
+export class PrismaMotorcycleRepository implements IMotorcycleRepository {
+  async save(motorcycle: Moto): Promise<void> {
+    await prisma.motorcycle.create({ data: { ...motorcycle } });
+  }
+
+  async update(licensePlate: string, data: Partial<Moto>): Promise<void> {
+    await prisma.motorcycle.update({ where: { licensePlate }, data });
+  }
+
+  async delete(licensePlate: string): Promise<void> {
+    await prisma.motorcycle.delete({ where: { licensePlate } });
+  }
+
+  async findByLicensePlate(licensePlate: string): Promise<Moto | null> {
+    return prisma.motorcycle.findUnique({ where: { licensePlate } });
   }
 
   async findAll(): Promise<Moto[]> {
-    return await prisma.moto.findMany();
+    return prisma.motorcycle.findMany();
   }
 }
