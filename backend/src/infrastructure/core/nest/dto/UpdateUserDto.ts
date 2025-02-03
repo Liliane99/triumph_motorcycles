581c2fe14704b374faaf48680abc89d0c6b1afaa
InesH2001 <ineshadidi08@gmail.com>
2025-02-03 12:11:16 +0100

    Feat: Add zod validator to DTOs

--- backend/src/infrastructure/core/nest/dto/UpdateUserDto.ts
+++ backend/src/infrastructure/core/nest/dto/UpdateUserDto.ts
@@ -1,9 +1,14 @@
-export class UpdateUserDto {
-    username?: string;
-    email?: string;
-    role?: "manager" | "client" | "admin";
-    phoneNumber?: string;
-    licenseNumber?: string;
-    experienceLevel?: string;
-}
-  
+import { z } from "zod";
+
+export const UpdateUserDtoSchema = z.object({
+  username: z.string().optional(),
+  email: z.string().email({ message: "L'email n'est pas valide." }).optional(),
+  role: z.enum(["manager", "client", "admin"], { 
+    errorMap: () => ({ message: "Le rôle doit être 'manager', 'client' ou 'admin'." }) 
+  }).optional(),
+  phoneNumber: z.string().optional(),
+  licenseNumber: z.string().optional(),
+  experienceLevel: z.string().optional(),
+});
+
+export type UpdateUserDto = z.infer<typeof UpdateUserDtoSchema>;
